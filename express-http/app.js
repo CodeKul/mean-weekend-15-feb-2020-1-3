4970e9d09b6b888be74d050251dd6fc167fa4706
aniruddha <aniruddha.kudalkar@gmail.com>
2020-03-01 10:36:24 +0530

    express : route and query params

--- express-http/app.js
+++ express-http/app.js
@@ -4,7 +4,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
-app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
+
+const jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.get('/', (request, response) => response.send('Hello World!'))
 app.get('/dtm', (request, response) => {
@@ -12,15 +18,22 @@ app.get('/dtm', (request, response) => {
 
     response.send({ date: new Date() })
 })
-app.post('/obj', (request, response) => {
+app.post('/obj', jsonParser, (request, response) => {
     console.log(request.body)
-    response.send({ obj : request.body, date : new Date() })
+    response.send({ obj: request.body, date: new Date() })
 })
-app.post('/calc', (req, res) => {
+app.post('/calc', jsonParser, (req, res) => {
     console.log(`Calculator`)
-    res.send({ result : eval(`${req.body['num1']}${req.body['op']}${req.body['num2']}`) })
+    res.send({ result: eval(`${req.body['num1']}${req.body['op']}${req.body['num2']}`) })
 })
-
+app.post('/form', urlencodedParser, (req, res) => {
+    console.log(`form`)
+    res.send(req.body.address)
+})
+app.get('/form/:id', urlencodedParser, (req, res) => {
+    console.log(`form`)
+    res.send({ routePrams: req.params, queryParams: req.query })
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 /**
