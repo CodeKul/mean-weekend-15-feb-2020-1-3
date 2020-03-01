e898bdffdc114d816e0677b4d33a524cde657238
aniruddha <aniruddha.kudalkar@gmail.com>
2020-03-01 11:19:55 +0530

    express : file upload

--- express-http/app.js
+++ express-http/app.js
@@ -2,8 +2,10 @@ const port = 3000
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload');
 
 const app = express()
+app.use(fileUpload());
 // app.use(bodyParser.urlencoded({ extended: false }))
 // app.use(bodyParser.json())
 
@@ -32,8 +34,26 @@ app.post('/form', urlencodedParser, (req, res) => {
 })
 app.get('/form/:id', urlencodedParser, (req, res) => {
     console.log(`form`)
+    res.set('Content-Type', 'application/json') //header
     res.send({ routePrams: req.params, queryParams: req.query })
 })
+
+app.post('/upload', function (req, res) {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    }
+
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    const sampleFile = req.files.sampleFile;
+
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv(`./${sampleFile.name}`, function (err) {
+        if (err)
+            return res.status(500).send(err);
+
+        res.send('File uploaded!');
+    });
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 /**
